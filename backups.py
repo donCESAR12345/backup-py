0d3fa18f9f19acbf9f15d6b6a7a9eb82f56eb3b7
donCESAR12345 <sprit152009@gmail.com>
2022-08-20 21:06:41 -0500

    Created main file, I should put a License here
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

---
+++ backups.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import argparse
+import os
+
+if __name__ == "__main__":
+    HOME = os.getenv("HOME")
+    GDRIVEDIR = f"{HOME}/GDrive"
+    BACKUPS_DIR = f"{HOME}/Backups"
+    KOPIATOKEN = 0
+    pass
